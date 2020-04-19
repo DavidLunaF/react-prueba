ec9b429b855b076f5bfa11e4de71c130c511abb4
David Luna <david.luna.fernandez@gmail.com>
2020-04-19 19:54:11 +0200

    working search & add card

--- src/App.js
+++ src/App.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './App.css';
 import Search from './components/Search';
 
+function success(pos) {
+  
+  const crd = pos.coords;
+};
+
+function error(err) {
+  console.warn('ERROR(' + err.code + '): ' + err.message);
+};
+
 function App() {
+  navigator.geolocation.getCurrentPosition(success, error);
+  const crd = '40.416775,-3.703790';
+
   return (
     <div className="App">
-      <Search />
+      <Search location={crd}/>
     </div>
   );
 }
--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import axios from 'axios';
+
 class Search extends React.Component {
+
+    apikey = process.env.REACT_APP_GOOGLE_API_KEY;
 
     constructor (props) {
         super(props);
-
+    
         this.state = {
             query: '',
             results: {},
@@ -17,20 +20,24 @@ class Search extends React.Component {
     
     getResults = ( updatedPageNum, query ) => {
         const pageNum = updatedPageNum;
-        const searchUrl = '';
-
+        const location = '';
+        const searchUrl = 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location='+ this.props.location +'&radius=1500'
+                            + '&keyword=' + query + '&key=' + this.apikey;
         if( this.cancel ) {
             this.cancel.cancel();
         }
 
         this.cancel = axios.CancelToken.source();
-
+        const config = {
+            headers: {'Access-Control-Allow-Origin': '*'}
+        };
         axios.get(searchUrl, {
-            cancelToken: this.cancel.token;
+            cancelToken: this.cancel.token
         }).then( response => {
-            const result = response;
-            console.log(response);
+            const result = response.data.results;
+            console.log(result);
         }).catch( error => {
+            console.log(error)
             if (axios.isCancel(error) || error) {
                 this.setState({
                     loading: false,
@@ -62,6 +69,7 @@ class Search extends React.Component {
                         placeholder="Busca Algo">
                         </input>
                 </label>
+            
             </div>
         );
     }
